14aa2d1da40af06ca6fea69e678a89867911ddac
chikeem90 <chikeem90@gmail.com>
2019-06-24 22:59:07 +0900

    Observer Pattern 커밋

---
+++ src/io/chikeem90/observerPattern/example/MinMaxView.java
@@ -0,0 +1,24 @@
+package io.chikeem90.observerPattern.example;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxView implements Observer {
+    private ScoreRecord scoreRecord;
+
+    public MinMaxView(ScoreRecord scoreRecord) {
+        this.scoreRecord = scoreRecord;
+    }
+
+    public void update() {
+        List<Integer> record = scoreRecord.getScoreRecord();
+        displayMinMax(record);
+    }
+
+    private void displayMinMax(List<Integer> record) {
+        int min = Collections.min(record, null);
+        int max = Collections.max(record, null);
+        System.out.println("Min : " + min + " Max : " + max);
+    }
+}
---
+++ src/io/chikeem90/observerPattern/example/Observer.java
@@ -0,0 +1,5 @@
+package io.chikeem90.observerPattern.example;
+
+public interface Observer {
+    public void update();
+}
